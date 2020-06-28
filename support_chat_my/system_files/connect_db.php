180b0a279cb66dea7ba7581d21bc080aa5467754
Developer-max96 <59201550+Developer-max96@users.noreply.github.com>
2020-06-28 12:05:56 +0300

    Update connect_db.php

--- support_chat_my/system_files/connect_db.php
+++ support_chat_my/system_files/connect_db.php
@@ -1,7 +1,7 @@
 <?php
-$host = 'localhost';
-$user_name = 'farrews0_modx';
-$pass = 'lBA8pb%n';
-$db_name = 'farrews0_modx';
+$host = 'localhost'; //хост базы данных как правило localhost
+$user_name = 'user_name'; //Имя пользователя базы данных
+$pass = 'db_password'; //пароль от базы данных
+$db_name = 'db_name'; //имя базы данных
 $link = mysqli_connect($host, $user_name, $pass, $db_name);
 ?>
