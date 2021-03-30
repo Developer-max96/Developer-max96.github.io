852a8eb0d7e10b282e13635ffd359d4a040d59ff
maxim.skvortsov <test@mail.ru>
2021-03-30 20:42:47 +0300

    обновление файла connectdb

--- support_chat_my/system_files/connect_db.php
+++ support_chat_my/system_files/connect_db.php
@@ -2,6 +2,6 @@
 $host = 'localhost'; //хост базы данных как правило localhost
 $user_name = 'user_name'; //Имя пользователя базы данных
 $pass = 'db_password'; //пароль от базы данных
-$db_name = 'db_name'; //имя базы данных
+$db_name = 'db_name'; //имя базы данных 
 $link = mysqli_connect($host, $user_name, $pass, $db_name);
 ?>
