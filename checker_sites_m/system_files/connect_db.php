0bba3c62d5bfbb9f597bf64f37d3c4c495e5ebaa
Developer-max96 <59201550+Developer-max96@users.noreply.github.com>
2021-09-09 17:48:37 +0300

    Update connect_db.php

--- checker_sites_m/system_files/connect_db.php
+++ checker_sites_m/system_files/connect_db.php
@@ -1,9 +1,9 @@
 <?php
 
 define('HOST', 'localhost'); //хост базы данных как правило localhost
-define('USER_NAME', 'pgd0211_allcorp'); //имя пользователя базы данных (логин)
-define('DB_PASSWORD', 'g*24&Zbn');//пароль базы данных
-define('DB_NAME', 'pgd0211_allcorp');//имя базы данных
+define('USER_NAME', 'db_user_name'); //имя пользователя базы данных (логин)
+define('DB_PASSWORD', 'db_pasword');//пароль базы данных
+define('DB_NAME', 'db_name');//имя базы данных
 
 $link = mysqli_connect(HOST, USER_NAME, DB_PASSWORD, DB_NAME);
 ?>
