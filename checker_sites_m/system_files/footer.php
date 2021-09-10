07e81d13523acb3ce7d126314d4a4049e72f7265
Developer-max96 <59201550+Developer-max96@users.noreply.github.com>
2021-09-10 09:35:09 +0300

    Update footer.php

--- checker_sites_m/system_files/footer.php
+++ checker_sites_m/system_files/footer.php
@@ -6,7 +6,7 @@ if(file_exists($_SERVER['DOCUMENT_ROOT'].'/system_files/functions.php'))
 ?>
 
 <footer>
-<div>
+<div <?if($_SERVER['REQUEST_URI']=='/'):?>class="main_footer"<?endif;?>>
     (c) 2021 Разработчик сервиса Скворцов М.А.
 </div>
 </footer>
