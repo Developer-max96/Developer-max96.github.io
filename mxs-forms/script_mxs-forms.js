2bcd15081dfc63800b0e3f2a01976fbef46004c4
maxim.skvortsov <test@mail.ru>
2021-09-12 11:20:57 +0300

    update admin and change password

--- mxs-forms/script_mxs-forms.js
+++
@@ -1,5 +0,0 @@
-$( document ).ready(function() 
-{
-	$('#text1_field_n1').attr('placeholder', 'Имя');
-	$('#text1_field_n2').attr('placeholder', 'Email');
-});
--- support_chat_my/system_files/connect_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = 'localhost'; //хост базы данных как правило localhost
-$user_name = 'user_name'; //Имя пользователя базы данных
-$pass = 'db_password'; //пароль от базы данных
-$db_name = 'db_name'; //имя базы данных 
-$link = mysqli_connect($host, $user_name, $pass, $db_name);
-?>
