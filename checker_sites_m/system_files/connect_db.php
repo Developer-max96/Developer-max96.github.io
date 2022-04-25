0c851638c2227dd588c159679597dd5cd1b51e13
developer.sites <test@mail.ru>
2022-04-25 19:41:01 +0300

    delete cecker

--- checker_sites_m/system_files/connect_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('HOST', 'localhost'); //хост базы данных как правило localhost
-define('USER_NAME', ''); //имя пользователя базы данных (логин)
-define('DB_PASSWORD', '');//пароль базы данных
-define('DB_NAME', '');//имя базы данных
-?>
